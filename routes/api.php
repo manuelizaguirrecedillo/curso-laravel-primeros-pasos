f831e483d956f0a29e40a62c2523b52b7f46bdb0
manuelizaguirrecedillo <87442008+manuelizaguirrecedillo@users.noreply.github.com>
2022-11-08 15:54:56 -0600

    laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//FORMA DIRECTA VALIDAR
+Route::group(['middleware' => 'auth:sanctum'],function(){
+Route::resource('category', CategoryController::class)->except(["create","edit"]);
+Route::resource('post', PostController::class)->except(["create","edit"]);
+});
+
+
+//form aindirecta validar
+
+Route::post('user/login', [UserController::class,'login']);
+
 Route::get('post/all', [PostController::class, 'all']);
 Route::get('post/slug/{post:slug}', [PostController::class, 'slug']);  //manera directa
 //Route::get('post/slug/{slug}', [PostController::class, 'slug']);    //primera manera
@@ -29,7 +41,3 @@ Route::get('category/slug/{slug}', [CategoryController::class, 'slug']);
 Route::get('category/{category}/posts', [CategoryController::class, 'posts']);
 
 
-
-
-Route::resource('category', CategoryController::class)->except(["create","edit"]);
-Route::resource('post', PostController::class)->except(["create","edit"]);
